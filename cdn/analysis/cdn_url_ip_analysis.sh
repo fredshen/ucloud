bcf4e746b1b84bde6bdba4bbebb036fca083f01d
Fred Shen <36216333+fredshen@users.noreply.github.com>
2021-02-02 13:33:24 +0800

    Update cdn_url_ip_analysis.sh

--- cdn/analysis/cdn_url_ip_analysis.sh
+++ cdn/analysis/cdn_url_ip_analysis.sh
@@ -45,7 +45,7 @@ gunzip ${log_name_00}.gz ${log_name_01}.gz ${log_name_02}.gz ${log_name_03}.gz
 cat ${log_name_00} ${log_name_01} ${log_name_02} ${log_name_03} > initial.log
 rm -rf ${log_name_00} ${log_name_01} ${log_name_02} ${log_name_03}
 #log_size=*
-echo -e "$(date +%Y-%m-%d\ %H:%M:%S)\nStep 1/4: 原始日志已合并完毕，即将进行URL排序..."
+echo -e "$(date +%Y-%m-%d\ %H:%M:%S) Step 1/4:\n原始日志已合并完毕，即将进行URL排序..."
 
 #截取日志文件的第5列（URL），并将其重复项合并，从大到小进行排序
 awk '{print $5}' initial.log | sort |uniq -c | sort -n -r |less > url_sort.log
@@ -53,7 +53,7 @@ head -20 url_sort.log > url_sort_top20.log
 head -3 url_sort_top20.log > url_sort_top3.log
 awk '{print $2}' url_sort_top3.log > top3_url.log
 rm -rf url_sort.log url_sort_top3.log
-echo -e "$(date +%Y-%m-%d\ %H:%M:%S)\nStep 2/4: URL已排序完成，即将从原始日志<initial.log>中获取TOP3URL的完整日志..."
+echo -e "$(date +%Y-%m-%d\ %H:%M:%S) Step 2/4:\nURL已排序完成，即将从原始日志<initial.log>中获取TOP3URL的完整日志..."
 
 #通过TOP3的URL，在原始日志<initial.log>中过滤，导出TOP3URL的原始日志<top3url_initial.log>
 cat top3_url.log | while read myline
@@ -61,13 +61,13 @@ do
         grep "$myline" initial.log > top3url_initial.log
 done
 rm -rf top3_url.log
-echo -e "$(date +%Y-%m-%d\ %H:%M:%S)\nStep 3/4: TOP3URL完整日志已获取完成，即将进行IP排序..."
+echo -e "$(date +%Y-%m-%d\ %H:%M:%S) Step 3/4:\nTOP3URL完整日志已获取完成，即将进行IP排序..."
 
 #将TOP3URL的原始日志top3url_initial.log处理，将请求IP从大到小一次排序
 awk '{print $3}' top3url_initial.log | sort |uniq -c | sort -n -r |less > top3url_ip_sort.log
 head -20 top3url_ip_sort.log > top3url_ip_sort_top20.log
 rm -rf top3url_initial.log top3url_ip_sort.log
-echo -e "$(date +%Y-%m-%d\ %H:%M:%S)\nStep 4/4: TOP3URL对应的IP已排序完成，所有任务已完成！！"
+echo -e "$(date +%Y-%m-%d\ %H:%M:%S) Step 4/4:\nTOP3URL对应的IP已排序完成，所有任务已完成！！"
 
 #获取任务结束时间，并计算耗时
 end_time=$(date +%s)
