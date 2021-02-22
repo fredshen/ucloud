2296e4c561d3f612a08dbc683dcc685b167eb2bd
Fred Shen <36216333+fredshen@users.noreply.github.com>
2021-02-22 16:30:13 +0800

    Update create_uhost_api.sh

--- uhost/create/create_uhost_api.sh
+++ uhost/create/create_uhost_api.sh
@@ -2,3 +2,20 @@
 #Author:F.Shen
 
 
+#客户的公钥，需要修改
+#PublicKey="kzje7oFRHFz6POmZ=="
+
+#客户的私钥，需要修改
+#PrivateKey="c1f28ce16d8a42"
+
+#客户URL编码后的公钥，需要修改
+#PublicKeyURL="kzje7oFRHFz6POmZhBhvqs"
+
+https://api.ucloud.cn/?Action=TerminateUHostInstance
+Region=cn-sh2
+Zone=cn-sh2-01
+ProjectId=org-mgdgnn
+UHostId=uhost-p2zqvesh
+ReleaseUDisk=true
+PublicKey=KWviTyPpwdyExhhnmxFCOhIHKRUOokJUElyHSbahQ
+Signature=?
