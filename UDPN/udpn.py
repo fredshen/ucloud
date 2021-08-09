0145a8e4456956d259e95e12e313f8555a777946
Fred Shen <36216333+fredshen@users.noreply.github.com>
2021-08-09 18:03:14 +0800

    是

---
+++ UDPN/udpn.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+ #!/usr/bin/env python3
+import requests
+import json
+import time,datetime
+
+month = int(input('输入要查询的月份：'))
+if month in [1,3,5,7,8,10,12]:
+  day = 31
+elif month in [4,6,9,11]:
+  day = 30
+else:
+  day = 28
+
+start_time =('20210'+str(month)+'01')
+startTime = time.mktime((time.strptime(start_time, "%Y%m%d")))
+startList = [];endList = [];y = []
+
+
+for x in range(day):
+  
+  startList.append(int(startTime+x*86400))
+  endList.append(int(startTime+(x+1)*86400))
+
+ 
+
+  url='http://internal.api.ucloud.cn'
+  data = {
+    "Backend": "UMON3-INNER-API",
+    "Action" : "GetMetricStats",
+    "AccountId" : 1,
+    "Metric" : 'resource.ustats2.thirdpart.unet_dpn_outbw',
+    "Uuid" : 'udpn-mnwjlcej',
+    "StartTime" : startList[x],
+    "EndTime" : endList[x],
+  }
+
+  res = requests.post(url,json=data).json()
+  res = json.dumps(res)
+  res = json.loads(res)
+
+  t = res['Data']['Results'][0]['Values']
+
+  
+  for x in t.values():
+    y.append(x)
+
+  y.sort(reverse=True)
+  a = int(len(y)*0.05)
+  print(y[a])
