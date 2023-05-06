edcdac9804284d284813f91792a9848d15579bd3
Fred Shen <shenchao74@163.com>
2023-05-06 10:40:59 +0800

    Update reinstall_OS.py

--- uhost/reinstall_OS/reinstall_OS.py
+++ uhost/reinstall_OS/reinstall_OS.py
@@ -9,24 +9,33 @@ from ucloud.core import exc
 from ucloud.client import Client
 import time
 
+#修改为个人秘钥
 pubulicKey = "4eZBKoQnYDiAjVI6SPmPiRH47Sj4Fhxlu"
 privateKey = "9AXyD8mINFgHlrblFp1tAPIcUBwfojXiBKgoPyEzgi6W"
 
-tmp_choice_imageId = input("选择需要重装的操作系统版本：\n1:CentOS 7.9\n2:CentOS 8.2\n3:Ubuntu 18.04\n4:Ubuntu 20.04\n5.Windows Server 2016\n\n你的选择是（直接输入数字）：")
-if tmp_choice_imageId == "1":
-	imageId = "uimage-naikvu"
-elif tmp_choice_imageId == "2":
-	imageId = "uimage-nr0z0a"
-elif tmp_choice_imageId == "3":
-	imageId = "uimage-icub4s"
-elif tmp_choice_imageId == "4":
-	imageId = "uimage-5onxu1"
-elif tmp_choice_imageId == "5":
-	imageId = "uimage-bxcdmu"
-else:
-	print("输入有误，请重新选择！\n""选择需要重装的操作系统版本：\n1:CentOS 7.9\n2:CentOS 8.2\n3:Ubuntu 18.04\n4:Ubuntu 20.04\n5.Windows Server 2016\n\n你的选择是（直接输入数字）：")
+#对操作系统版本进行选择
+tmp_choice_imageId = 100
+while tmp_choice_imageId > 5 or tmp_choice_imageId < 1:
+	tmp_choice_imageId = int(input("选择需要重装的操作系统版本：\n1:CentOS 7.9\n2:CentOS 8.2\n3:Ubuntu 18.04\n4:Ubuntu 20.04\n5.Windows Server 2016\n\n你的选择是（直接输入数字）："))
+	if tmp_choice_imageId == 1:
+		imageId = "uimage-naikvu"
+		OS = "CentOS 7.9"
+	elif tmp_choice_imageId == 2:
+		imageId = "uimage-nr0z0a"
+		OS = "CentOS 8.2"
+	elif tmp_choice_imageId == 3:
+		imageId = "uimage-icub4s"
+		OS = "Ubuntu 18.04"
+	elif tmp_choice_imageId == 4:
+		imageId = "uimage-5onxu1"
+		OS = "Ubuntu 20.04"
+	elif tmp_choice_imageId == 5:
+		imageId = "uimage-bxcdmu"
+		OS = "Windows Server 2016"
+	else:
+		print("------------------------------\n输入有误，请重新选择！\n")
 
-
+#对实力进行关机并重装系统
 def main():
 	client = Client({
 	"region": "cn-sh2",
@@ -35,6 +44,7 @@ def main():
 	"project_id": "org-fye3q5",
 	"base_url": "https://api.ucloud.cn"
 })
+	#关机
 	try:
 		resp = client.uhost().stop_uhost_instance({
 			"Zone": "cn-sh2-02",
@@ -48,7 +58,7 @@ def main():
 	print("正在关机。。。")
 	time.sleep(10)
 	print("------------------------------\n关机成功，正在重装系统。。。")
-
+	#重装系统
 	try:
 		resp = client.uhost().reinstall_uhost_instance({
 			"Zone": "cn-sh2-02",
@@ -61,7 +71,7 @@ def main():
 		print(e)
 	else:
 		print(resp)
-		print("------------------------------\n系统重装成功，密码为：SS**2@")
+		print("------------------------------\n系统重装成功!\n操作系统版本为："+ OS +"\n密码为：SS**2@")
 
 if __name__ == '__main__':
 	main()
