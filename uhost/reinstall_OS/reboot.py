2025ef42dfdc97ef62377b02a89b6d2228915c4d
Fred Shen <shenchao74@163.com>
2023-05-08 09:57:24 +0800

    Create reboot.py

---
+++ uhost/reinstall_OS/reboot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Homepage: https://github.com/ucloud/ucloud-sdk-python3
+Examples: https://github.com/ucloud/ucloud-sdk-python3/tree/master/examples
+Document: https://docs.ucloud.cn/opensdk-python/README
+"""
+from ucloud.core import exc
+from ucloud.client import Client
+
+pubulicKey = "4eXvtltJL2h30eiaGMPsi37tu0Wi2UKFz"
+privateKey = "29SOSFBqNRR5lvo5gKQ5ovAuFXZjNQoXJ1vHSsHokY40"
+
+def main():
+	client = Client({
+	"region": "cn-sh2",
+	"public_key": pubulicKey,
+	"private_key": privateKey,
+	"project_id": "org-ww0gsd",
+	"base_url": "https://api.ucloud.cn"
+})
+	
+	try:
+		resp = client.uhost().reboot_uhost_instance({
+			"Zone": "cn-sh2-01",
+			"UHostId": "uhost-fwylm23y"
+			
+		})
+	except exc.UCloudException as e:
+		print(e)
+	else:
+		print(resp)
+		
+if __name__ == '__main__':
+	main()
+	
