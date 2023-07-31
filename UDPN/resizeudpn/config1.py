7886dc41c918dd559889acd51c8881780917ce91
Fred Shen <shenchao74@163.com>
2023-07-31 09:56:14 +0800

    1

---
+++ UDPN/resizeudpn/config1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#修改为个人秘钥
+#账号：shenchaook@gmail.com
+pubulicKey = "4eZBKoQnYDiAjVI6SPmPiRH47Sj4Fhxlu"
+privateKey = "9AXyD8mINFgHlrblFp1tAPIcUBwfojXiBKgoPyEzgi6W"
+projectId = "org-fye3q5"
+udpnId = "udpn-lygdi78yzxf"#修改为个人ID
+#old_bandwidth = "10000"#随意设置原始值
+#bandoutUsage = 100
+
+def jy():
+	print('demo')
