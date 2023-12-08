a569fc3a3ed6479397ced8e79b701ad27e3f2949
Fred Shen <shenchao74@163.com>
2023-12-08 10:01:35 +0800

    1

--- api_test/udpn_shenchaook_test.py
+++ api_test/udpn_shenchaook_test.py
@@ -12,13 +12,25 @@ gz:
 106.75.148.6/10.13.182.248
 hk:
 118.193.43.181/10.7.123.156
+
+getmetric
+参数	描述信息	单位
+BandOut	出带宽	b/s
+BandOutMax	峰值出带宽	b/s
+BandOutUsage	带宽使用率	％
+BandOutMaxUsage	峰值带宽使用率	％
 '''
 
-current_bandwith = 2
+Current_Bandwith = 2
 #当前购买带宽值
-new_bandwith = 3
+New_Bandwith = 3
 #新的购买带宽值
-current_bandwith_usage = 60
+Current_Bandwith_Usage = 60
 #当前带宽使用率
 modification_range = 15
+
+Log_Path = "/Users/fredshen/Downloads/uwork/API_test"
+
 #调整幅度
+Band_Out_Max = "1"
+Band_Out_Max_Usage = "1"
---
+++ api_test/原始/GetMetric_BandOutMaxUsage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+"""
+Homepage: https://github.com/ucloud/ucloud-sdk-python3
+Examples: https://github.com/ucloud/ucloud-sdk-python3/tree/master/examples
+Document: https://docs.ucloud.cn/opensdk-python/README
+"""
+from ucloud.core import exc
+from ucloud.client import Client
+
+#修改为个人秘钥
+#账号：shenchaook@gmail.com
+pubulicKey = "4eZBKoQnYDiAjVI6SPmPiRH47Sj4Fhxlu"
+privateKey = "9AXyD8mINFgHlrblFp1tAPIcUBwfojXiBKgoPyEzgi6W"
+
+client = Client({
+	"public_key": pubulicKey,
+	"private_key": privateKey,
+	"project_id": "Input project ID at here",
+	"base_url": "https://api.ucloud.cn"
+})
+
+try:
+	resp = client.invoke("GetMetric", {
+		"Region": "cn-gd",
+		"Zone": "cn-gd-02",
+		"ProjectId": "org-fye3q5",
+		"ResourceType": "udpn",
+		"ResourceId": "udpn-r3qv8h1dkau",
+		"TimeRange": 200,
+		"MetricName.0": "BandOutMaxUsage"
+})
+except exc.RetCodeException as e:
+	resp = e.json()
+	
