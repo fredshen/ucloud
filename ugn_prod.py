8cc5543d2eb608ab4547d3f1830bbdc197ba20a7
Fred Shen <shenchao74@163.com>
2023-12-13 10:10:14 +0800

    1

--- ugn_prod.py
+++ ugn_prod.py
@@ -24,55 +24,58 @@ client = Client({
 	"base_url": "https://api.ucloud.cn"
 })
 
+def main():	
+	#获取当前带宽值
+	resp1 = client.invoke("GetSimpleUGNBwPackages", {
+		"ProjectId": Project_ID,
+		"UGNID": UGN_ID
+	})
 	
-#获取当前带宽值
-resp1 = client.invoke("GetSimpleUGNBwPackages", {
-	"ProjectId": Project_ID,
-	"UGNID": UGN_ID
-})
-
-Current_Bandwith = resp1.get("BwPackages")[-1].get("BandWidth")
-print(Current_Bandwith)
-
-#获取监控值
-resp2 = client.invoke("GetMetric", {
-	"Region": "cn-sh2",
-	"Zone": "cn-sh2-01",
-	"ProjectId": "org-en5c1p",
-	"ResourceType": "ugnbw",
-	"ResourceId": "bw-qv7mfeyujk3",
-	"TimeRange": 500,
-	"MetricName.0": "UgnBWOutPeakUsage"
-})
-
-Current_UgnBWOut_PeakUsage = resp2.get("DataSets").get("UgnBWOutPeakUsage")[-1].get("Value")
-print(Current_UgnBWOut_PeakUsage)
-
-#判断
-if Current_UgnBWOut_PeakUsage < 60:
-	New_Bandwith = int(Current_Bandwith * 0.8)
-	if New_Bandwith < 2:
-		New_Bandwith = 2
-	else:
-		New_Bandwith = New_Bandwith *1
-elif Current_UgnBWOut_PeakUsage > 60 and Current_UgnBWOut_PeakUsage < 80:
-	New_Bandwith = int(Current_Bandwith * 0.9)
-	if New_Bandwith < 2:
-		New_Bandwith = 2
-	else:
-		New_Bandwith = New_Bandwith *1
-else:
-	New_Bandwith = int(Current_Bandwith * Current_UgnBWOut_PeakUsage *1.2 / 100 + 1)
+	Current_Bandwith = resp1.get("BwPackages")[-1].get("BandWidth")
+	print(Current_Bandwith)
 	
-#调整带宽值
-resp = client.invoke("ModifyUGNBandwidth", {
-	"ProjectId": "org-en5c1p",
-	"PackageID": "bw-qv7mfeyujk3",
-	"UGNID": "ugn-qr34zqz4xuh",
-	"BandWidth": New_Bandwith
-})
+	#获取监控值
+	resp2 = client.invoke("GetMetric", {
+		"Region": "cn-sh2",
+		"Zone": "cn-sh2-01",
+		"ProjectId": "org-en5c1p",
+		"ResourceType": "ugnbw",
+		"ResourceId": "bw-qv7mfeyujk3",
+		"TimeRange": 500,
+		"MetricName.0": "UgnBWOutPeakUsage"
+	})
+	
+	Current_UgnBWOut_PeakUsage = resp2.get("DataSets").get("UgnBWOutPeakUsage")[-1].get("Value")
+	print(Current_UgnBWOut_PeakUsage)
+	
+	#判断
+	if Current_UgnBWOut_PeakUsage < 60:
+		New_Bandwith = int(Current_Bandwith * 0.8)
+		if New_Bandwith < 2:
+			New_Bandwith = Current_Bandwith + 1
+		else:
+			New_Bandwith = New_Bandwith *1
+	elif Current_UgnBWOut_PeakUsage > 60 and Current_UgnBWOut_PeakUsage < 80:
+		New_Bandwith = int(Current_Bandwith * 0.9)
+		if New_Bandwith < 2:
+			New_Bandwith = Current_Bandwith + 1
+		else:
+			New_Bandwith = New_Bandwith *1
+	else:
+		New_Bandwith = int(Current_Bandwith * Current_UgnBWOut_PeakUsage *1.2 / 100 + 1)
+		
+	#调整带宽值
+	resp = client.invoke("ModifyUGNBandwidth", {
+		"ProjectId": "org-en5c1p",
+		"PackageID": "bw-qv7mfeyujk3",
+		"UGNID": "ugn-qr34zqz4xuh",
+		"BandWidth": New_Bandwith
+	})
+	
+	print("---aaaa----")
+	print("当前带宽使用率："+ str(Current_UgnBWOut_PeakUsage))
+	print("当前带宽购买值："+ str(Current_Bandwith))
+	print("新购买值："+str(New_Bandwith))
 
-print("---aaaa----")
-print("当前带宽使用率："+ str(Current_UgnBWOut_PeakUsage))
-print("当前带宽购买值："+ str(Current_Bandwith))
-print("新购买值："+str(New_Bandwith))
+if __name__ == "__main__":
+	main()
