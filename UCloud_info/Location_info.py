4c14806dffa0cbcc708d5ddc06dafae9d60abccf
Fred Shen <shenchao74@163.com>
2023-12-22 10:43:20 +0800

    1

---
+++ UCloud_info/Location_info.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+#华北一：
+Region = "cn-bj2"
+Zone = "cn-bj2-02"#华北一B
+Zone = "cn-bj2-03"#华北一C
+Zone = "cn-bj2-04"#华北一D
+Zone = "cn-bj2-05"#华北一D
+
+#华北一：
+Region = "cn-wlcb"
+Zone = "cn-wlcb-01"#华北二A
+
+#上海二：
+Region = "cn-sh2"
+Zone = "cn-sh2-01"#上海二A
+Zone = "cn-sh2-02"#上海二B
+Zone = "cn-sh2-03"#上海二C
+
+#广州
+Region = "cn-gd"
+Zone = "cn-gd-02"#广州B
+
+#香港
+Region = "hk"
+Zone = "hk-01"#香港A
+Zone = "hk-02"#香港B
+
+#台北
+Region = "tw-tp"
+Zone = "tw-tp-01"#台北A
+
+#东京
+Region = "jpn-tky"
+Zone = "jpn-tky-01"#东京A
+
+#首尔
+Region = "kr-seoul"
+Zone = "kr-seoul-01"#首尔A
+
+#新加坡
+Region = "sg"
+Zone = "sg-01"#新加坡A
+Zone = "sg-02"#新加坡B
+
+#法兰克福
+Region = "ge-fra"
+Zone = "ge-fra-01"#法兰克福A
+
+#洛杉矶
+Region = "us-ca"
+Zone = "us-ca-01"#洛杉矶A
+
+#莫斯科
+Region = "rus-mosc"
+Zone = "rus-mosc-01"#莫斯科A
