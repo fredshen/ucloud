5392dc304238262e3fd555008bb34255971054b8
Fred Shen <36216333+fredshen@users.noreply.github.com>
2024-04-13 14:48:05 +0800

    1

---
+++ LinkRoad/test.py
@@ -0,0 +1,7 @@
+#修改为个人秘钥
+#账号：it@link-road.com
+Pubulic_Key = "4eZBe1MqPe4DGoVXowkRzQCWvNqPWYORa"
+Private_Key = "AgflYW5XCrZ8hJrwH8e3dLFSWyIz6hzqIJH7YmC8wwwR"
+Project_ID = "org-2xvcdf"#Default
+
+print("hello world")
